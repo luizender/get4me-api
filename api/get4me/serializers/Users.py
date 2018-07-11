e86d631a93adec3197b237bb6b853b305d97809f
Luiz Gustavo Ender <luiz.ender@intelbras.com.br>
2018-07-11 14:51:13 -0300

    Added unique validation of User.

--- api/get4me/serializers/Users.py
+++ api/get4me/serializers/Users.py
@@ -1,6 +1,27 @@
 from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
 from django.contrib.auth import get_user_model
 from django.contrib.auth.validators import UnicodeUsernameValidator
+from django.utils.translation import ugettext_lazy as _
+
+class UserUniqueValidator(UniqueValidator):
+
+    message = _('A user with that username already exists.')
+
+    def set_context(self, serializer_field):
+        self.field_name = serializer_field.source_attrs[-1]
+
+        user_serializer = serializer_field.parent
+        if not user_serializer:
+            return
+
+        model_serializer = user_serializer.parent
+        if not model_serializer:
+            return
+
+        instance = getattr(model_serializer, 'instance', None)
+        if instance:
+            self.instance = instance.user
 
 class UserSerializer(serializers.ModelSerializer):
 
@@ -11,7 +32,10 @@ class UserSerializer(serializers.ModelSerializer):
         )
         extra_kwargs = {
             'username': {
-                'validators': [UnicodeUsernameValidator()]
+                'validators': [
+                    UnicodeUsernameValidator(),
+                    UserUniqueValidator(queryset=get_user_model().objects.all())
+                ]
             },
             'password': {
                 'write_only': True
@@ -23,6 +47,7 @@ class ModelHasUserSerializer(serializers.ModelSerializer):
 
     user = UserSerializer(required=True)
 
+
     def create(self, validated_data):
         user = get_user_model().objects.create_user(**validated_data.pop('user'))
         instance = self.Meta.model.objects.create(user=user, **validated_data)
@@ -31,11 +56,13 @@ class ModelHasUserSerializer(serializers.ModelSerializer):
     def update(self, instance, validated_data):
         user_data = validated_data.pop('user')
         if user_data:
-            instance.user.first_name = user_data['first_name']
-            instance.user.last_name = user_data['last_name']
-            instance.user.username = user_data['username']
-            instance.user.email = user_data['email']
-            instance.user.set_password(user_data['password'])
+            for attr, value in user_data.items():
+                if attr is 'password':
+                    instance.user.set_password(value)
+                    continue
+
+                setattr(instance.user, attr, value)
+
             instance.user.save()
 
         for attr, value in validated_data.items():
